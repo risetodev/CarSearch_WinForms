eb002fd4d5806643098bce8e806cac14ed24b588
Ruslan Molchaniuk <ram14apple@gmail.com>
2017-05-07 00:27:58 +0300

    Add files via upload

---
+++ CarSearch/CarSearch/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class CarSearch
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CarSearch));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(376, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(785, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 77);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(956, 568);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(129, 123);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(757, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(363, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(757, 248);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(363, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(736, 312);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(384, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(747, 376);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(373, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(926, 429);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(194, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(875, 488);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(245, 20);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 163);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(522, 532);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(739, 543);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(189, 20);
+            this.textBox8.TabIndex = 10;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // CarSearch
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1128, 705);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CarSearch";
+            this.Text = "CarSearch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ CarSearch/CarSearch/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class CarSearch : Form
+    {
+        public class Car
+        {
+            public string mark;
+            public string model;
+            public string type;
+            public string color;
+            public string sizeOfWheel;
+            public string horsepower;
+            public string year;
+            public Car(string mark, string model, string type, string color, string sizeOfWheel, string horsepower, string year)
+            {
+                this.mark = mark;
+                this.model = model;
+                this.type = type;
+                this.color = color;
+                this.sizeOfWheel = sizeOfWheel;
+                this.horsepower = horsepower;
+                this.year = year;
+            }
+            public static List<Car> createCarsList(string path)
+            {
+                List<Car> CarList = new List<Car>();
+                StreamReader readCarList = new StreamReader(@path);
+                var listOfCars = readCarList;                
+                string lc = listOfCars.ToString();
+                string cars = CarList.ToString();
+                string s = null;
+                foreach (var i in lc)
+                {
+                    while ((s = listOfCars.ReadLine()) != null)
+                    {
+                        string[] CarsSplit = s.Split(' ');
+                        Car newCar = new Car(CarsSplit[0], CarsSplit[1], CarsSplit[2], CarsSplit[3], CarsSplit[4], CarsSplit[5], CarsSplit[6]);
+                        CarList.Add(newCar);
+                    }                    
+                }
+                readCarList.Close();
+                return CarList;                
+            }                      
+            public override string ToString()
+            {
+                string outString = null;
+                outString = "Mark ::: " + mark + "\nModel ::: " + model + "\nType ::: " + type + "\nColor ::: " + color + "\nSizeOfWheel ::: " + sizeOfWheel + "\nHorsepower ::: " + horsepower + "\nYear ::: " + year + "\n--------------------------------------------------------------------------------------------------------------------------------\n";
+                return outString;
+            }        
+        }      
+        public CarSearch()
+        {
+            InitializeComponent();
+        }         
+        public string way = "Cars.txt";
+        public string newCar;
+        public string splNewCar;
+        public string mark;
+        public string model;
+        public string type;
+        public string color;
+        public string sizeOfWheel;
+        public string horsepower;
+        public string year;        
+        public string featcher;               
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+        }        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            featcher = textBox1.Text.ToString();     
+        }       
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            List<Car> Cars = Car.createCarsList(way);
+            if (string.IsNullOrEmpty(featcher))
+            {
+                MessageBox.Show("You didn't write anything!");
+                return;
+            }          
+            richTextBox1.Clear();
+            foreach (Car i in Cars)
+            {
+                if (i.mark == featcher || i.model == featcher || i.type == featcher || i.color == featcher || i.sizeOfWheel == featcher || i.horsepower == featcher || i.year == featcher)
+                {
+                    richTextBox1.Text += i.ToString();
+                }                
+            }            
+        }       
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.ReadOnly = true;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            mark = textBox2.Text.ToString();
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            model = textBox3.Text.ToString();
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            type = textBox4.Text.ToString();
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            color = textBox5.Text.ToString();
+        }
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            sizeOfWheel = textBox6.Text.ToString();
+        }
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            horsepower = textBox7.Text.ToString();
+        }
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            year = textBox8.Text.ToString();
+        } 
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {            
+            if (string.IsNullOrEmpty(mark) && string.IsNullOrEmpty(model) && string.IsNullOrEmpty(type) && string.IsNullOrEmpty(color) && string.IsNullOrEmpty(sizeOfWheel) && string.IsNullOrEmpty(horsepower) && string.IsNullOrEmpty(year))
+            {
+                MessageBox.Show("You didn't write anything!");
+                return;
+            }
+            if (string.IsNullOrEmpty(mark) || string.IsNullOrEmpty(model) || string.IsNullOrEmpty(type) || string.IsNullOrEmpty(color) || string.IsNullOrEmpty(sizeOfWheel) || string.IsNullOrEmpty(horsepower) || string.IsNullOrEmpty(year))
+            { 
+                MessageBox.Show("Something you miss!!!");
+            }
+            else  using (StreamWriter addNewCar = File.AppendText(way))
+                {
+                    newCar = (mark + " " + model + " " + type + " " + color + " " + sizeOfWheel + " " + horsepower + " " + year);
+                    splNewCar = newCar;
+                    newCar.ToString().Split(' ');
+                    addNewCar.WriteLine(newCar);
+                    MessageBox.Show("Successfully saved!!!"); 
+                }                      
+        }                        
+    } 
+}
